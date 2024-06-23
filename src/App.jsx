6d314e4128d8b9fab3227a848aead684f59916ff
Dominic Mushayi <dominicmushayi@Dominics-MacBook-Pro.local>
2024-06-23 22:50:34 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import { About, Contact, Home, Projects } from "./pages";
+
+const App = () => {
+  return (
+    <main className='bg-slate-300/20'>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/About' element={<About />} />
+          <Route path='/Projects' element={<Projects />} />
+          <Route path='/Contact' element={<Contact />} />
+        </Routes>
+      </Router>
+    </main>
+  );
+};
+
+export default App;
