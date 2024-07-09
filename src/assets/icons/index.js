83faaee5952f8dcf575fde5ffac65fd4635a0ab7
Dominic <mushayidominic@gmail.com>
2024-07-09 11:40:35 +0200

    Initial commit

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,21 +1,12 @@
 import css from './css.svg'
-import express from './express.svg'
 import git from './git.svg'
 import github from './github.svg'
 import html from './html.svg'
 import javascript from './javascript.svg'
-import mongodb from './mongodb.svg'
-import motion from './motion.svg'
-import mui from './mui.svg'
-import nextjs from './nextjs.svg'
 import nodejs from './nodejs.svg'
 import react from './react.svg'
-import redux from './redux.svg'
-import sass from './sass.svg'
 import tailwindcss from './tailwindcss.svg'
-import typescript from './typescript.svg'
 import linkedin from './linkedin.svg'
-import twitter from './twitter.svg'
 import car from './car.svg'
 import estate from './estate.svg'
 import pricewise from './pricewise.svg'
@@ -26,34 +17,44 @@ import arrow from './arrow.svg'
 import contact from './contact.svg'
 import soundon from './soundon.png'
 import soundoff from './soundoff.png'
+import java from './java.svg';
+import python from './python.svg';
+import tensorflow from './tensorflow.svg';
+import mysql from './mysql.svg';
+import laravel from './laravel.svg';
+import netlify from './netlify.svg';
+import c from './c.svg';
+import postgresql from './postgresql.svg'
+
+
+
 
 export {
+    arrow,
+    car,
+    contact,
     css,
-    express,
+    estate,
+    c,
     git,
     github,
     html,
     javascript,
-    mongodb,
-    motion,
-    mui,
-    nextjs,
-    nodejs,
-    react,
-    redux,
-    sass,
-    tailwindcss,
-    typescript,
     linkedin,
-    twitter,
-    car,
-    estate,
+    soundoff,
+    nodejs,
     pricewise,
+    react,
+    soundon,
     snapgram,
     summiz,
+    tailwindcss,
     threads,
-    arrow,
-    contact,
-    soundon,
-    soundoff
+    python,
+    java,
+    tensorflow,
+    netlify,
+    laravel,
+    mysql,
+    postgresql
 }
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+/* eslint-disable react/jsx-key */
+import React from 'react';
+import { skills } from '../constants';
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <section className='max-container'>
+      <h1 className='head-text'>
+        Hello, I'm <span className='blue-gradient_text font-semibold drop-shadow'>Dominic</span> 🦦
+      </h1>
 
-export default About
+      <div>
+        <p className='mt-5 flex flex-col gap-3 text-slate-500'>
+          I am a student currently studying Computer Science with a focus on Machine Learning and Artificial Intelligence.
+          I am passionate about how machines can learn from data and make intelligent decisions, and I've explored various aspects of AI through my coursework and projects.
+        </p>
+      </div>
+
+      <div className='py-10 flex flex-col'>
+        <h3 className='subhead-text'>My Skills</h3>
+        <div className='mt-16 flex flex-wrap gap-12'>
+          {skills.map((skill, ) => (
+            <div className='block-container w-20 h-20' >
+              <div  className='btn-back rounded-xl'/>
+              <div className='btn-front rounded-xl flex justify-center items-center'>
+              <img
+                src={skill.imageUrl}
+                alt={skill.name}
+                className='w-1/2 h-1/2 object-contain'
+              />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
